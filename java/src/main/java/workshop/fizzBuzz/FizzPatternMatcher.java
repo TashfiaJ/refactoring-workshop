8b4806a3beaf1e18c732e564e1c3b6333641dd5b
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-12-27 11:51:42 +0600

    Update FizzPatternMatcher.java

--- java/src/main/java/workshop/fizzBuzz/FizzPatternMatcher.java
+++ java/src/main/java/workshop/fizzBuzz/FizzPatternMatcher.java
@@ -1,6 +1,6 @@
 package fizzBuzz;
 
-public class FizzPatternMatcher implements PatternMatcher{
+public class FizzPatternMatcher implements IDivisibilityChecker{
 
     @Override
     public boolean matches(int number) {
