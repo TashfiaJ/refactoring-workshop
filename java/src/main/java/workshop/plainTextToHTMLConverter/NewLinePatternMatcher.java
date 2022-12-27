001beeee7dd22d79f6ef99cc0fcf58a6545d240e
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-12-27 12:06:40 +0600

    Update NewLinePatternMatcher.java

--- java/src/main/java/workshop/plainTextToHTMLConverter/NewLinePatternMatcher.java
+++ java/src/main/java/workshop/plainTextToHTMLConverter/NewLinePatternMatcher.java
@@ -1,6 +1,6 @@
 package plainTextToHTMLConverter;
 
-public class NewLinePatternMatcher implements PatternMatcher{
+public class NewLinePatternMatcher implements IPatternMatcher{
     @Override
     public boolean matches(String plainText) {
         return plainText.equals("\n");
