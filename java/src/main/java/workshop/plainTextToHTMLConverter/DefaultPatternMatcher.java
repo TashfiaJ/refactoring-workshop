ea95aee9687ba758b66f4f9d10abc2eb77b1ae27
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-12-27 12:04:30 +0600

    Update DefaultPatternMatcher.java

--- java/src/main/java/workshop/plainTextToHTMLConverter/DefaultPatternMatcher.java
+++ java/src/main/java/workshop/plainTextToHTMLConverter/DefaultPatternMatcher.java
@@ -1,6 +1,6 @@
 package plainTextToHTMLConverter;
 
-public class DefaultPatternMatcher implements PatternMatcher{
+public class DefaultPatternMatcher implements IPatternMatcher{
     private String strReturn;
     @Override
     public boolean matches(String plainText) {
